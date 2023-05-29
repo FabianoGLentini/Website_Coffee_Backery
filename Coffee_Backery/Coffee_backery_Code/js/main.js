83b51aac63a1ad3da8d95ddfe646f7deff8c1c2b
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-05-29 13:46:29 -0400

    Add: parallax effects for both large background img using JS

--- Coffee_Backery/Coffee_backery_Code/js/main.js
+++ Coffee_Backery/Coffee_backery_Code/js/main.js
@@ -11,3 +11,20 @@
 // function hoverGlow(){
 //     document.querySelector(".hoverGlow li").style.text-decoration: "underline";
 // }
+
+
+/******************************************
+/* Parallax
+/*******************************************/
+
+const parallax = document.getElementById("parallax") 
+const parallax02 = document.getElementById("parallax02") 
+
+
+window.addEventListener("scroll", paralaxScroll)
+
+function paralaxScroll(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 +"px"
+    parallax02.style.backgroundPositionY = offset * 0.7 +"px"
+}
