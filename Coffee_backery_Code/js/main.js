e50a9c3bedb9a128019337b5e95e53e3d5b5d7f9
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-09-21 09:51:55 -0400

    WIP: rework mobile nav bar symbol

--- Coffee_backery_Code/js/main.js
+++ Coffee_backery_Code/js/main.js
@@ -41,8 +41,11 @@ const navToggle = document.querySelector(".mobile-nav-toggle")
 navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible")
 
-    console.log(visibility)
     if(visibility === "false"){
         primaryNav.setAttribute("data-visible" , true)  
+        navToggle.setAttribute("aria-expanded", true)
+      }else{
+        primaryNav.setAttribute("data-visible" , false)  
+        navToggle.setAttribute("aria-expanded", false)
       }
 })
