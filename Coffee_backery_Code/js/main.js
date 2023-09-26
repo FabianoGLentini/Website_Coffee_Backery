3d2fa7657a306c7267373a5fe07676c25da8808b
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-09-26 11:31:04 -0400

    WIP: js code for the product display carousel

--- Coffee_backery_Code/js/main.js
+++ Coffee_backery_Code/js/main.js
@@ -30,7 +30,6 @@ function paralaxScroll(){
     parallax02.style.backgroundPositionY = offset * 0.7 +"px"
 }
 
-
 /******************************************
 /* Nav Bar Toggle
 /*******************************************/
@@ -49,3 +48,22 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-expanded", false)
       }
 })
+
+/******************************************
+/* Carousel Product Display
+/*******************************************/
+
+const track = document.querySelector(".carousel__track")
+const slides = Array.from(track.children)
+const nextButton = document.querySelector(".carousel__button--right")
+const prevButton = document.querySelector(".carousel__button--left")
+const dotsNav = document.querySelector(".carousel__nav--bottom")
+const dots = Array.from(dotsNav.children)
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+console.log(slideWidth)
+
+// When I click left, move slides to the left
+// When I click right, move slides to the right
+// When I click the nav indicators, move to that slide
