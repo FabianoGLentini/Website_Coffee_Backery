bef637ad389294602f5fdb236109d7558bf867e9
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-09-26 13:48:50 -0400

    Add: carousel spin on right arrow key click for the porduct display

--- Coffee_backery_Code/js/main.js
+++ Coffee_backery_Code/js/main.js
@@ -62,8 +62,27 @@ const dots = Array.from(dotsNav.children)
 
 const slideWidth = slides[0].getBoundingClientRect().width
 
-console.log(slideWidth)
+// console.log(slideWidth)
+
+// Arrange the slides next to one another
+
+slides.forEach(setSlidePosition)
+
+function setSlidePosition(slides, i) {
+  slides.style.left = slideWidth * i + "px"
+}
 
 // When I click left, move slides to the left
 // When I click right, move slides to the right
+nextButton.addEventListener("click", e => {
+  const currentSlide = track.querySelector(".current-slide")
+  const nextSlide = currentSlide.nextElementSibling
+  const amountToMove = nextSlide.style.left
+
+  // const move to the next slide
+  track.style.transform = "translateX( -" + amountToMove + ")"
+  currentSlide.classList.remove("current-slide")
+  nextSlide.classList.add("current-slide")
+})
+
 // When I click the nav indicators, move to that slide
