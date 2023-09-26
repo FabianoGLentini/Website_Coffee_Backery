2aa40b59ce1b8a84fd9a08901520a71e5c80ba78
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-09-26 16:03:52 -0400

    Add: carousel arrow disappear as we go on either end of the ul li list

--- Coffee_backery_Code/js/main.js
+++ Coffee_backery_Code/js/main.js
@@ -62,8 +62,6 @@ const dots = Array.from(dotsNav.children)
 
 const slideWidth = slides[0].getBoundingClientRect().width
 
-// console.log(slideWidth)
-
 // Arrange the slides next to one another
 
 slides.forEach(setSlidePosition)
@@ -78,12 +76,36 @@ function moveToSlide(track, currentSlide, targetSlide){
   targetSlide.classList.add("current-slide")
 }
 
+function updateDots(currentDot, targetDot){
+  currentDot.classList.remove("current-slide")
+  targetDot.classList.add("current-slide")
+}
+
+function hideShowArrows(slides, prevButton, nextButton, targetIndex){
+  if(targetIndex === 0) {
+    prevButton.classList.add("is-hidden")
+    nextButton.classList.remove("is-hidden")
+  }else if(targetIndex === slides.length-1){
+    prevButton.classList.remove("is-hidden")
+    nextButton.classList.add("is-hidden")
+  }else{
+    prevButton.classList.remove("is-hidden")
+    nextButton.classList.remove("is-hidden")
+  }
+}
+
 // When I click left, move slides to the left
 prevButton.addEventListener("click", e => {
   const currentSlide = track.querySelector(".current-slide")
   const prevSlide = currentSlide.previousElementSibling
+  const currentDot = dotsNav.querySelector(".current-slide")
+  const prevDot = currentDot.previousElementSibling
+  const prevIndex = slides.findIndex(slide => slide === prevSlide)
 
   moveToSlide(track, currentSlide, prevSlide)
+  updateDots(currentDot, prevDot)
+  hideShowArrows(slides, prevButton, nextButton, prevIndex)
+
 })
 
 
@@ -91,8 +113,13 @@ prevButton.addEventListener("click", e => {
 nextButton.addEventListener("click", e => {
   const currentSlide = track.querySelector(".current-slide")
   const nextSlide = currentSlide.nextElementSibling
+  const currentDot = dotsNav.querySelector(".current-slide")
+  const nextDot = currentDot.nextElementSibling
+  const nextIndex = slides.findIndex(slide => slide === nextSlide)
 
   moveToSlide(track, currentSlide, nextSlide)
+  updateDots(currentDot, nextDot)
+  hideShowArrows(slides, prevButton, nextButton, nextIndex)
 })
 
 // When I click the nav indicators, move to that slide
@@ -111,8 +138,7 @@ dotsNav.addEventListener("click", e => {
   const targetSlide = slides[targetIndex]
 
   moveToSlide(track, currentSlide, targetSlide)
-
-  currentDot.classList.remove("current-slide")
-  currentDot.classList.add("current-slide")
+  updateDots(currentDot, targetDot)
+  hideShowArrows(slides, prevButton, nextButton, targetIndex)
 })
 
